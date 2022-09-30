7983786993d4366dfc1016d35d1d5d0032b9be96
syawalamir <syawalamir24@gmail.com>
2022-09-30 17:26:43 +0700

    update

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -3,9 +3,11 @@ class contoh1 extends CI_Controller
 {
 	public function index()
 	{
-		echo "<h1> Perkenalkan";
+		echo "<h1> Perkenalkan </h1>";
 		echo "<br>";
-		echo "Nama Saya Amir Syawaludin
-		saya tinggal di Jakarta";
+		echo "Nama Saya Imam Nawawi 
+		saya tinggal di daerah ciputat
+		olah raga yang saya sukai adalah
+		bulutangkis";
 	}
 }
