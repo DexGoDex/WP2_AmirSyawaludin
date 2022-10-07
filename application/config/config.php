7b33b07c3567d152ea761b5398aa6094ce6f3750
syawalamir <syawalamir24@gmail.com>
2022-10-07 17:27:52 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Latihan/WP2_AmirSyawaludin/Matakuliah';
+$config['base_url'] = 'http://localhost/Latihan/WP2_AmirSyawaludin/';
 
 /*
 |--------------------------------------------------------------------------
